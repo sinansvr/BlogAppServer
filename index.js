32bd7bf5a7e422555a8fb279b84059bb7478ddc3
sinans <svrsinan06@gmail.com>
2023-11-20 11:29:35 +0300

    password

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ require('dotenv').config()
 require("express-async-errors")
 const PORT = process.env.PORT || 8000
 const HOST = process.env.HOST || '127.0.0.1'
+app.use(express.json())
 
 
 //DB Connection
---
+++ src/models/user.js
@@ -0,0 +1,91 @@
+"use strict"
+
+const { Schema, model } = require("mongoose")
+const { default: isEmail } = require("validator/lib/isEmail")
+
+const{isEmail}=require("validator")
+const passwordEncrypt = require("../helpers/passwordEncrypt")
+
+// {
+//     "username": "admin",
+//     "password": "aA*123456",
+//     "email": "admin@site.com",
+//     "first_name": "admin",
+//     "last_name": "admin",
+//     "image":"",
+//     "bio":"",
+//     "isAdmin": true
+// }
+// {
+//     "username": "staff",
+//     "password": "aA*123456",
+//     "email": "staff@site.com",
+//     "first_name": "staff",
+//     "last_name": "staff",
+//     "image":"",
+//     "bio":"",
+//     "isAdmin": false
+// }
+// {
+//     "username": "test",
+//     "password": "aA*123456",
+//     "email": "test@site.com",
+//     "first_name": "test",
+//     "last_name": "test",
+//     "image":"",
+//     "bio":"",
+//     "iAdmin": false
+// }
+
+// User Model:
+const UserSchema=new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
+    },
+    
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true,
+        validator:[isEmail,"Email type is not correct"]
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        set:(password)=>passwordEncrypt(password)
+    },
+    first_name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    last_name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    image: {
+        type: String,
+        trim: true,
+        required: false
+    },
+    bio: {
+        type: String,
+        trim: true,
+        required: false
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+​
+},{ collection: 'users', timestamps: true })
+/* ------------------------------------------------------- */
+// Schema Configs:
