0ba70639fe138c11eeb349c6062b4a1ce83bd0cd
sinans <svrsinan06@gmail.com>
2023-11-20 10:58:40 +0300

    fixed

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 module.exports=(err,req,res,next)=>{
 
-    return res.status(res.errorStatusCode || 500).send({
+    return res.status(res?.errorStatusCode || 500).send({
         error:true,
         message:err.message,
         cause:err.cause,
