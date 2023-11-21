cc54063728e46299fdd8e546c2c1c4c09ee561c1
sinans <svrsinan06@gmail.com>
2023-11-21 11:40:03 +0300

    route

---
+++ src/routes/blogComLikCat.js
@@ -0,0 +1,60 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESS_JS - BLOG-API Router
+------------------------------------------------------- */
+const router = require("express").Router();
+/* ------------------------------------------------------- */
+// routes/users:
+
+const category = require("../controllers/category")
+const blog = require("../controllers/blog")
+
+
+// category routes
+router.route("/categories")
+  .get(category.list).post(category.create);
+
+router
+  .route("/categories/:id")
+  .get(category.read)
+  .put(category.update)
+  .patch(category.update)
+  .delete(category.delete);
+
+
+// comment routes
+router.route("/comments")
+  .get(category.list).post(category.create);
+
+router
+  .route("/comments/:id")
+  .get(category.read)
+  .put(category.update)
+  .patch(category.update)
+  .delete(category.delete);
+
+// blog  routes
+router.route("/blogs")
+  .get(blog.list).post(blog.create);
+
+router
+  .route("/blogs/:id")
+  .get(blog.read)
+  .put(blog.update)
+  .patch(blog.update)
+  .delete(blog.delete);
+
+// likes  routes
+router.route("/likes")
+  .get(category.list).post(category.create);
+
+router
+  .route("/likes/:id")
+  .get(category.read)
+  .put(category.update)
+  .patch(category.update)
+  .delete(category.delete);
+
+
+/* ------------------------------------------------------- */
+module.exports = router;
