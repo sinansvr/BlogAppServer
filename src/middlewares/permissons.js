4b1787d616015101697574507e4ffd7e2f8469b1
sinans <svrsinan06@gmail.com>
2023-11-23 11:10:13 +0300

    node env

--- src/middlewares/permissons.js
+++ src/middlewares/permissons.js
@@ -1,19 +1,30 @@
 "use strict";
+/* -------------------------------------------------------
+    NODEJS EXPRESS | CLARUSWAY FullStack Team
+------------------------------------------------------- */
+// Middleware: permissions
 
 module.exports = {
-  isLogin: (req, res, next) => {
-    if (req?.user) next();
-    else {
-      res.errorStatusCode = 403;
-      throw new Error("No Permissoons, You must login!");
-    }
-  },
-  isAdmin: (req, res, next) => {
+    isLogin: (req, res, next) => {
+        if (process.env.NODE_ENV == "development") return next();
 
-    if (req?.user?.isAdmin) next();
-    else {
-      res.errorStatusCode = 403;
-      throw new Error("No Permissoons, You must login and to be Admin!");
-    }
-  },
+        if (req.user) {
+            next();
+        } else {
+            res.errorStatusCode = 403;
+            throw new Error("NoPermission: You must login.");
+        }
+    },
+
+
+    isAdmin: (req, res, next) => {
+        if (process.env.NODE_ENV == "development") return next();
+
+        if (req.user && req.user.isAdmin) {
+            next();
+        } else {
+            res.errorStatusCode = 403;
+            throw new Error("NoPermission: You must login and to be Admin.");
+        }
+    },
 };
